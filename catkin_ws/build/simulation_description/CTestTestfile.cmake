d3630a081093dba6d52b82b97746e2dc108ae470
PJaskula <jaskula.przemek@gmail.com>
2022-11-23 08:50:05 -0800

    Added catkin_ws

---
+++ catkin_ws/build/simulation_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pszemek/Desktop/catkin_ws/src/simulation_description
+# Build directory: /home/pszemek/Desktop/catkin_ws/build/simulation_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/simulation_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pszemek/Desktop/catkin_ws/src/simulation_gazebo
+# Build directory: /home/pszemek/Desktop/catkin_ws/build/simulation_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
