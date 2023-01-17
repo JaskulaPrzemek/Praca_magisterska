cb46c0ff4ca4b981974f0e55d01942d9e23e040f
PJaskula <jaskula.przemek@gmail.com>
2023-01-17 08:59:21 -0800

    Finished python conversion

--- pythonLol/gazeboCommunication.py
+++ pythonLol/gazeboCommunication.py
@@ -27,6 +27,15 @@ class GazeboCommunication(metaclass=SingletonMeta):
 		self.cmd_publisher=rospy.Publisher('/pioneer2dx/cmd_vel', Twist, queue_size=2)
 		self.cmd=Twist()
 		self.modelList=[]
+		self.angMax=0.3
+		self.linMax=0.3
+		self.Kp=3
+		self.Ki=0.02
+		self.Kd=0.5
+		self.linear_error_sum=0
+		self.angular_error_sum=0
+		self.linear_prev_error=0
+		self.angular_prev_error=0
 	
 	def callback_odom(self,msg):
 		self.robot_x=msg.pose.pose.position.x
@@ -37,7 +46,6 @@ class GazeboCommunication(metaclass=SingletonMeta):
 		self.robot_angle=table_of_euler_angles[2]
    	
 	def callback_range(self,msg):
-		print(msg)
 		self.robot_range=msg.range
    	
 	def spawnPioneer(self,x,y):
@@ -110,4 +118,58 @@ class GazeboCommunication(metaclass=SingletonMeta):
 		initial_pose.orientation.z = 0
 		spawn_model_prox = rospy.ServiceProxy('gazebo/spawn_sdf_model', SpawnModel)
 		spawn_model_prox(tname, model.toxml(), '', initial_pose, 'world')
-
+	def publish(self):
+		if self.cmd.angular.z >self.angMax:
+				self.cmd.angular.z=self.angMax
+		if self.cmd.angular.z <-self.angMax:
+				self.cmd.angular.z=-self.angMax
+		if self.cmd.linear.x >self.linMax:
+				self.cmd.linear.x=self.linMax
+		if self.cmd.linear.x <-self.linMax:
+				self.cmd.linear.x=-self.linMax
+		self.cmd_publisher.publish(self.cmd)
+	def goToPoint(self,desired,target):
+		posBeg=(int(self.robot_x) , int(self.robot_y))
+		x=self.robot_x - desired[0]
+		y=self.robot_y - desired[1]
+		error_angle = angles.shortest_angular_distance(angles.normalize_angle(self.robot_angle), math.atan2(y, x))
+		while error_angle > math.pi/30:
+			self.cmd.angular.z=self.Kp*error_angle
+			self.publish()
+		dist_to_goal = math.sqrt(x**2 + y**2)
+		flag=0
+		while abs(dist_to_goal) > 0.05:
+			x=self.robot_x - desired[0]
+			y=self.robot_y - desired[1]
+			error_angle = angles.shortest_angular_distance(angles.normalize_angle(self.robot_angle), math.atan2(y, x))
+			dist_to_goal = math.sqrt(x**2 + y**2)
+			self.cmd.angular.z=self.Kp*error_angle
+			self.cmd.linear.x=self.Kp*dist_to_goal
+			self.publish()
+			if robot_range < 0.3:
+				flag=1
+				self.driveBackwards(posBeg)
+				break
+		self.cmd.angular.z=0
+		self.cmd.linear.x=0
+		if flag:
+			return -1,posBeg
+		if desired == target:
+			return 2,desired
+		return 0,desired
+		
+	def driveBackwards(self,target):
+		x=self.robot_x - target[0]
+		y=self.robot_y - target[1]
+		dist_to_goal = math.sqrt(x**2 + y**2)
+		error_angle = angels.normalize(angles.shortest_angular_distance(angles.normalize_angle(self.robot_angle), math.atan2(y, x)) -pi)
+		while abs(dist_to_goal) > 0.05:
+			x=self.robot_x - target[0]
+			y=self.robot_y - target[1]
+			dist_to_goal = math.sqrt(x**2 + y**2)
+			error_angle = angels.normalize(angles.shortest_angular_distance(angles.normalize_angle(self.robot_angle), math.atan2(y, x)) -pi)
+			self.cmd.angular.z=self.Kp*error_angle
+			self.cmd.linear.x=-self.Kp*dist_to_goal
+			self.publish()
+		self.cmd.angular.z=0
+		self.cmd.linear.x=0
